f8e70360af6647a85859b40ffacd49efd5c968af
Mario Prieto <mprietopereira@gmail.com>
2024-05-30 14:46:56 +0200

    chore: add new types of toast messages (success, error, warning, info) and update web

--- web/src/components/Toaster.tsx
+++ web/src/components/Toaster.tsx
@@ -1,11 +1,14 @@
 export const positions = ['top-left', 'top-center', 'top-right', 'bottom-left', 'bottom-center', 'bottom-right'] as const
+export const types = ['success', 'error', 'warning', 'info'] as const
 
 export type Position = (typeof positions)[number]
+export type Type = (typeof types)[number]
 
 type ToasterProps = {
   position: Position
+  richColors?: boolean
 }
 
-export const Toaster = ({ position }: ToasterProps) => {
-  return <section id="toaster-wrapper" data-position={position} />
+export const Toaster = ({ position, richColors }: ToasterProps) => {
+  return <section id="toaster-wrapper" data-position={position} data-rich-colors={richColors} />
 }
